7565845698cb0d613aa65b1bfdda956628fe45d4
samil2 <samil.korkmaz@gmail.com>
2026-08-03 12:42:52 +0300

    Allow any IP to connect

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(title="Kizilay Demo OMR Prototype")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origin_regex=r"https?://(localhost|\d{1,3}(\.\d{1,3}){3})(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
