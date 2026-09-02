b60bbdc01379527107047222cfb08e1fb578d1d5
Pelin Keskin <pelinkeskin@kizilay.org.tr>
2026-09-02 09:14:51 +0300

    Redesign frontend visual system

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata, Viewport } from "next";
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/500.css";
+import "@fontsource/ibm-plex-sans/600.css";
+import "@fontsource/ibm-plex-sans/700.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/600.css";
 import "./globals.css";
 
 export const metadata: Metadata = {
